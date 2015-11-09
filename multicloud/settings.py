e4c4d9e695049f303febf85dbd3d711b21e69029
Michael Storm <mstorm7@gmail.com>
2015-11-09 00:23:08 -0800

    Set STATIC_ROOT to 'staticfiles'

--- multicloud/settings.py
+++ multicloud/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/tmp/multicloud_static/'
+STATIC_ROOT = 'staticfiles'
 
 PROJECT_DIR = os.path.dirname(__file__)
 
