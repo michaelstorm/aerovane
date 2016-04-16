9fccbebc01ec440bfe054a77fa58d206ef04fe99
Michael Storm <mstorm7@gmail.com>
2016-04-16 15:46:52 -0700

    Prevent autofill on non-Aerovane password fields

--- stratosphere/forms.py
+++ stratosphere/forms.py
@@ -8,6 +8,9 @@ Ec2ProviderConfigurationForm = modelform_factory(Ec2ProviderConfiguration, field
 
 LinodeProviderConfigurationForm = modelform_factory(LinodeProviderConfiguration, fields=('api_key',), widgets={'api_key': PasswordInput, 'user_configuration': HiddenInput})
 
-PasswordAuthenticationMethodForm = modelform_factory(PasswordAuthenticationMethod, fields=('name', 'password'), widgets={'password': PasswordInput, 'user_configuration': HiddenInput})
+PasswordAuthenticationMethodForm = modelform_factory(PasswordAuthenticationMethod,
+                                                     fields=('name', 'password'),
+                                                     widgets={'password': PasswordInput(attrs={'autocomplete':'new-password'}),
+                                                               'user_configuration': HiddenInput})
 
 KeyAuthenticationMethodForm = modelform_factory(KeyAuthenticationMethod, fields=('name', 'key'), widgets={'user_configuration': HiddenInput})
