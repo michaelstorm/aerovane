9eb4107424d7a087ef15698b9891520c6f062429
Michael Storm <mstorm7@gmail.com>
2016-05-05 23:48:26 -0700

    Ask for first and last name when registering

--- stratosphere/forms.py
+++ stratosphere/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm, PasswordInput, HiddenInput
 from django.forms.models import modelform_factory
 
@@ -14,3 +15,13 @@ PasswordAuthenticationMethodForm = modelform_factory(PasswordAuthenticationMetho
                                                                'user_configuration': HiddenInput})
 
 KeyAuthenticationMethodForm = modelform_factory(KeyAuthenticationMethod, fields=('name', 'key'), widgets={'user_configuration': HiddenInput})
+
+
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=30, label='First name')
+    last_name = forms.CharField(max_length=30, label='Last name')
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
